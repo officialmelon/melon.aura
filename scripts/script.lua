b2d8509595d1cedecc8c48c39e313722a5c30938
Melon <80179069+OfficiallyMelon@users.noreply.github.com>
2025-09-14 00:25:28 +1000

    gurt

---
+++ scripts/script.lua
@@ -0,0 +1,16 @@
+--// socials
+gurt.select("#github"):on('click', function()
+    gurt.location.goto('https://github.com/officialmelon')
+end)
+
+gurt.select("#reddit"):on('click', function()
+    gurt.location.goto('https://www.reddit.com/user/BraxyBo/')
+end)
+
+gurt.select("#yt"):on('click', function()
+    gurt.location.goto('https://www.youtube.com/@Melon-Fruit')
+end)
+
+gurt.select("#type.shit"):on('click', function()
+    gurt.location.goto('gurt://type.shit')
+end)
