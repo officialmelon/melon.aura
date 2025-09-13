c1874e9a38869e4aa401c70909a9d85b11d352ca
Melon <80179069+OfficiallyMelon@users.noreply.github.com>
2025-09-14 00:54:04 +1000

    cool tween

--- scripts/script.lua
+++ scripts/script.lua
@@ -14,3 +14,23 @@ end)
 gurt.select("#type.shit"):on('click', function()
     gurt.location.goto('gurt://type.shit')
 end)
+
+--// tween in
+
+function elementCleanLoad(element, tweenTime)
+	setTimeout(function()
+		element:createTween():to('opacity', 0):duration(0):play()
+		Time.sleep(0.01)
+		element:createTween():to('opacity', 1):duration(tweenTime):play()
+	end, 0)
+end
+
+elementCleanLoad(gurt.select("#header"), 1)
+elementCleanLoad(gurt.select("#semiheader"), 1)
+elementCleanLoad(gurt.select("#info"), 1)
+elementCleanLoad(gurt.select("#sites"), 1)
+elementCleanLoad(gurt.select("#type.shit"), 1)
+
+elementCleanLoad(gurt.select("#github"), 1)
+elementCleanLoad(gurt.select("#reddit"), 1)
+elementCleanLoad(gurt.select("#yt"), 1)
